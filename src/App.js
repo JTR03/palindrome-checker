7ff96e3abb10f45cba65fa589428a74bcce96f85
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-01-14 11:59:34 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+ const [text, setText] = useState('')
+ const [check, setCheck] = useState('')
+
+ const handleInput = (e)=>{
+   e.preventDefault()
+   setText(e.target.value)
+ }
+
+ const handleCheck = (e)=>{
+   e.preventDefault()
+   const word = text.toLowerCase().split('')
+   let joint = word.join('')
+   let reversed = word.reverse().join('')
+
+   if(joint === reversed){
+     setCheck("Yes, it's a palindrome")
+   }else{
+     setCheck("Sorry, that's not palindrome")
+   }
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Palindrome Checker</h3>
+      <form onSubmit={handleCheck}>
+        <label htmlFor='word'>Palindrome: </label>
+        <input 
+        type='text'
+        name='word'
+        id='word'
+        value={text}
+        onChange={handleInput}/>
+        <button type='submit'>Check</button>
+      </form>
+      <p>
+        {check}
+      </p>
     </div>
   );
 }
